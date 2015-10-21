24c6865f8c03ce70daeb7a52b0d99f5edcc1b616
kyfxbl <kyfxbl@gmail.com>
2015-10-22 01:22:00 +0800

    读取topo.json

--- test/script/sorest/index.js
+++ test/script/sorest/index.js
@@ -1,7 +1,12 @@
+var path = require("path");
+var sep = path.sep;
+
 var Sorest = require("../../../lib/sorest");
 
-var sorest = new Sorest();
+var base_dir = path.join(process.cwd(), "test", "dummy") + sep;
+
+var sorest = new Sorest(base_dir);
 
 sorest.init();
 
-sorest._call("start", {});
+console.log(sorest.topo);
