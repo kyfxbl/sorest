cf0d0dfcc233a0e5d7d7b99bf235defdf8626c54
kyfxbl <kyfxbl@gmail.com>
2015-10-22 01:25:25 +0800

    更新测试脚本，去掉多余的变量

--- test/script/sorest/index.js
+++ test/script/sorest/index.js
@@ -4,8 +4,11 @@ var sep = path.sep;
 var Sorest = require("../../../lib/sorest");
 
 var base_dir = path.join(process.cwd(), "test", "dummy") + sep;
+var args = {
+    debug: true
+};
 
-var sorest = new Sorest(base_dir);
+var sorest = new Sorest(base_dir, args);
 
 sorest.init();
 
